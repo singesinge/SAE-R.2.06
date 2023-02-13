5cc42e5038909563c3ec66f626fa46c4cb0f7806
LeoG <103144119+LeoG1904@users.noreply.github.com>
2023-02-13 14:39:40 +0100

    léo
    réajustement des pop-up

--- JS/parasite.js
+++ JS/parasite.js
@@ -14,6 +14,7 @@ function createMessage(){
     p.innerHTML = lmessage[Math.floor(Math.random() * lmessage.length)]
     div.onclick = div.remove
     div.classList.add("spam-pop-up")
+    div.style.zIndex = 2;
     div.style.top = Math.floor(Math.random() * 40) + 5 + "%";
     div.style.left = Math.floor(Math.random() * 65) + 5 + "%";
     document.getElementById("ecran").appendChild(div);
