dcf3d783a526d567756795c6029054a08d4af8cd
mathiasbamas <mathias.bamas@etu.univ-amu.fr>
2023-02-17 00:23:16 +0100

    ajout mdp twitter

--- JS/script.js
+++ JS/script.js
@@ -17,6 +17,13 @@ function updateTime() {
 
   }
 
+function verifMdpDocument(){
+  const date = document.getElementById("dateDocPass")
+  if(date.value === "2007-03-25"){
+    ouvre('folderDocument')
+    fermer('passwordDocument')
+  }
+}
 
 function changePostIt(){
   var postIt1 = document.getElementById('recto');
@@ -35,13 +42,13 @@ function verifMdpTwitter(){
   var username = document.getElementById('usernameTwitter');
   var password = document.getElementById('passwordTwitter');
 
-  if(username.value === "test" && password.value === "test"){
+  if(username.value === "Doumbaaa" && password.value === "croustibate30"){
     var connectionWindow = document.getElementById('motDePasseTwitter');
     var pageTwitter = document.getElementById('appTwitter');
     connectionWindow.style.display = "none"
     pageTwitter.style.display = "block"
   }else{
-    alert("pas reussit")
+    username.value = "Doumbaaa"
     password.value = ""
   }
 }
@@ -65,16 +72,51 @@ function verifMdp(){
 
 const position = { x: 0, y: 0 }
 
-interact('.draggable').draggable({
-  listeners: {
-    start (event) {
-      console.log(event.type, event.target)
-    },
-    move (event) {
-      position.x += event.dx
-      position.y += event.dy
-      event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
-    },
-  }
-})
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        var textEl = event.target.querySelector('p')
+
+        textEl && (textEl.textContent =
+          'moved a distance of ' +
+          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px')
+      }
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
 
